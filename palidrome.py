e8026577307f7272e834b9e5e3492a29029d6511
WilsonJerez <83123385+wilwil186@users.noreply.github.com>
2021-10-18 15:12:47 -0500

    Add files via upload

---
+++ palidrome.py
@@ -0,0 +1,14 @@
+from typing import Annotated
+
+
+def is_palindrome(string: str) -> bool:
+    string = string.replace(' ', '').lower()
+    return string == string[::-1]
+
+ 
+def run():
+    print(is_palindrome(10000))
+
+
+if __name__ == '__main__':
+    run()
